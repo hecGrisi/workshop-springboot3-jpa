e90e543a1b5d40b10ae9f6f24e8ba8ccc4c09109
Hector Ferreira <hecgrisi@gmail.com>
2024-08-15 17:52:23 -0300

    Service layer, component registration

---
+++ src/main/java/wothan/enterprise/course/services/UserService.java
@@ -0,0 +1,26 @@
+package wothan.enterprise.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import wothan.enterprise.course.entities.User;
+import wothan.enterprise.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
