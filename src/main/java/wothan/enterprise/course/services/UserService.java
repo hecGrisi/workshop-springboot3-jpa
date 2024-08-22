e4acc3349d97c009f62ae664091754f317abb72e
Hector Ferreira <hecgrisi@gmail.com>
2024-08-22 16:56:22 -0300

    Exception handling - findById

--- src/main/java/wothan/enterprise/course/services/UserService.java
+++ src/main/java/wothan/enterprise/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import wothan.enterprise.course.entities.User;
 import wothan.enterprise.course.repositories.UserRepository;
+import wothan.enterprise.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
