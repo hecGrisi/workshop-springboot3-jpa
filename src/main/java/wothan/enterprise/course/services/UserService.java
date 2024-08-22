adc58b7ceb0805e9bf0d5bc908693ff044781b50
Hector Ferreira <hecgrisi@gmail.com>
2024-08-22 17:34:12 -0300

    Exception handling - update

--- src/main/java/wothan/enterprise/course/services/UserService.java
+++ src/main/java/wothan/enterprise/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import jakarta.persistence.EntityNotFoundException;
 import wothan.enterprise.course.entities.User;
 import wothan.enterprise.course.repositories.UserRepository;
 import wothan.enterprise.course.services.exceptions.DatabaseException;
@@ -45,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
